ae9b244f1be0ca5bf52e77a6d876435738d01393
Mallory Shea <malloryshea@Mallorys-MacBook-Air.local>
2022-01-13 12:30:04 -0600

    middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,42 @@
+const User = require('./../users/users-model')
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log({
+    method: req.method,
+    url: req.url,
+    time: new Date().toISOString()
+  })
+  next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id)
+    if (!user){
+      res.status(404).json({ message: "user not found" })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch(err) {
+    next(err)
+  }
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  if (!req.body.name || !req.body.name.trim()){
+    next({status: 400, message: "missing required name field" })
+  } else {
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  if (!req.body.text || !req.body.text.trim()){
+    next({status: 400, message: "missing required text"})
+  } else {
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
