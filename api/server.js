3cb3c29545329b43ed29a95bc12f0c24beaa0435
Mallory Shea <malloryshea@Mallorys-MacBook-Air.local>
2022-01-13 12:29:04 -0600

    wired app

--- api/server.js
+++ api/server.js
@@ -1,13 +1,39 @@
 const express = require('express');
+const { logger } = require('./middleware/middleware');
 
 const server = express();
 
-// remember express by default cannot parse JSON in request bodies
+server.use(express.json())
 
-// global middlewares and the user's router need to be connected here
+const usersRouter = require('./users/users-router')
+
+//MOVED TO MIDDLEWARE FILE, leaving for reference
+
+// server.use((req, res, next) => {
+//   console.log({
+//     method: req.method,
+//     url: req.url,
+//     time: new Date().toISOString()
+//   })
+//   next()
+// })
+
+server.use(logger)
+
+server.use('/api/users', usersRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
+
+server.use('*', (req, res) => {
+  res.status(404).json({message: `${req.method} ${req.baseUrl} not found!`})
+})
+
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: process.env.NODE_ENV === 'PROD'? 'sorry, there was an error' : err.message,
+  })
+})
 
 module.exports = server;
